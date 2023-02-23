18d145555ec9a24d9f5023ce967509bb319bb5cb
Shubham Tambwe <shubhamtambwe2018@gmail.com>
2023-02-23 13:35:05 +0530

    added functionality and features.

--- src/Component/TodoItem.jsx
+++ src/Component/TodoItem.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { useState } from "react";
 
-const TodoList = ({taskTest}) => {
-  const [isClicked, setIsCliked] = useState(true);
-  console.log(taskTest);
+const TodoList = ({taskName,taskStatus,taskCreatedTime,taskCompletedTime}) => {
+  const [isClicked, setIsCliked] = useState(false);
+  console.log(isClicked);
 
   return (
     <div className="task-item">
       <div className="task-text">
-        <h2>Write a Letter</h2>
-        <input type="checkbox" onChange={() => setIsCliked({setIsCliked : false})} />
+        <h2 className={isClicked ? "strike":""}>{taskName}</h2>
+        <input type="checkbox" onClick={()=> setIsCliked(!isClicked)} />
         <button>Delete</button>
       </div>
+
       <div className="task-text-heading">
-        <p>T create time</p>
-        <p>T completion time</p>
+        <p>{taskCreatedTime}</p>
+        <p>{taskCompletedTime}</p>
       </div>
     </div>
   );
--- src/Component/TodoList.jsx
+++ src/Component/TodoList.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const tasks = [
+    {
+      taskName: "Task 1",
+      taskStatus: 0,
+      taskCreatedTime: "",
+      taskCompletedTime: "",
+    },
+    {
+      taskName: "Task 2",
+      taskStatus: 0,
+      taskCreatedTime: "",
+      taskCompletedTime: "",
+    },
+    {
+      taskName: "Task 3",
+      taskStatus: 0,
+      taskCreatedTime: "",
+      taskCompletedTime: "",
+    },
+  ];
+
   return (
     <>
-        <TodoItem taskTest = "Hello" />
+      {tasks.map((task) => {
+        return (
+          <TodoItem
+            taskName={task.taskName}
+            taskStatus={task.taskStatus}
+            taskCreatedTime = {task.taskCreatedTime}
+            taskCompletedTime = {task.taskCompletedTime}
+          />
+        );
+      })} 
     </>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
