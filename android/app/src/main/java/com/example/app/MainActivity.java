3ac1ee3a62810ce2f679b7f44039da6ffcd8e02b
manishapillai77@gmail.com <manishapillai77@gmail.com>
2023-10-17 16:26:35 +0530

    android rebuilt and added sqlite hook

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.example.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+
+        //this.initialPlugins.add(CapacitorSQL)
+        //
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import TodoList from './Component/TodoList';
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./Component/TodoList";
+import { useSQLite, SQLiteHook } from "react-sqlite-hook";
+
+export let sqlite;
 
 function App() {
+  sqlite = useSQLite();
+  console.log("available :" + sqlite.isAvailable);
+
   return (
     <div className="App">
       <TodoList />
